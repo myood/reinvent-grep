0e60c75e811f5f4c9478cb780af3af05e41fcfbb
myood <myoodster@gmail.com>
2023-01-03 10:34:06 +0100

    Add cloning a git2-rs repository into temporary directory (framework to prepare data for integration tests)

--- tests/matching_file_names.rs
+++ tests/matching_file_names.rs
@@ -2,12 +2,57 @@
 #[macro_use]
 extern crate quickcheck;
 
-
 #[cfg(test)]
 mod tests {
+    use std::process::Command;
+    use git2::Repository;
+    use tempfile::TempDir;
+
     quickcheck! {
         fn environment_test(xs: bool) -> bool {
             xs == !(!xs)
         }
+    }
+
+    trait PathStr {
+        fn path_str(&self) -> String;
+    }
+
+    impl PathStr for TempDir {
+        fn path_str(&self) -> String {
+            self.path().to_string_lossy().to_owned().to_string()
+        }
+    }
+
+    struct TemporaryRepository {
+        dir: tempfile::TempDir
+    }
+
+    impl TemporaryRepository {
+        fn new(url: &'static str) -> TemporaryRepository {
+            let td = tempfile::tempdir().unwrap();
+            let path = td.path_str();
+            let _ = Repository::clone(url, &path).unwrap();
+            assert!(std::fs::read_dir(&path).is_ok());
+            TemporaryRepository { dir: td }
+        }
+    }
+
+    #[test]
+    fn clone_test() {
+        let mut path = String::new();
+        {
+            let tmp = TemporaryRepository::new("https://github.com/alexcrichton/git2-rs");
+            path = tmp.dir.path_str();
+        }
+        if !path.is_empty() {
+            assert!(std::fs::read_dir(&path).is_err());
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn failed_clone_test() {
+        TemporaryRepository::new("xxx https://github.com/alexcrichton/git2-rs");
     }
 }
