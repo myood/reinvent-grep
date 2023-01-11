9e2e74599ee2d4d01610bdb4aa8368d7b4867633
myood <myoodster@gmail.com>
2023-01-11 15:46:41 +0100

    Use RIO to leverage io_uring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::BufReader;
-use std::io::prelude::*;
+use std::io::{Read, BufRead, BufReader};
+use std::io::Result;
 use std::fs::File;
 use crossbeam_channel::{Sender, Receiver};
 use std::thread::{self, JoinHandle};
@@ -60,9 +60,65 @@ fn does_file_match(fd: File, substr:& str) -> bool {
         })
 }
 
-fn parse_file_with_string(fd: File, path: &str, substr: &str) -> Vec<String> {
+struct RioFile {
+    fd: File,
+    rio_ring: rio::Rio,
+    position: u64,
+    buffer: Vec<u8>,
+}
+
+impl RioFile {
+    fn from(fd: File, ring: &rio::Rio) -> RioFile {
+        RioFile { fd: fd, rio_ring: ring.clone(), position: 0u64, buffer: vec![] }
+    }
+}
+
+impl Read for RioFile {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let completion = self.rio_ring.read_at(&self.fd, &buf, self.position);
+        match completion.wait() {
+            Ok(bytes_read) => {
+                self.position += bytes_read as u64;
+                Ok(bytes_read)
+            },
+            Err(e) => Err(e)
+        }
+    }
+}
+
+impl BufRead for RioFile {
+    fn fill_buf(&mut self) -> Result<&[u8]> {
+        if self.buffer.len() != 0 {
+            return Ok(self.buffer.as_slice())
+        }
+
+        let mut b = [0u8; 4096]; 
+        match self.read(&mut b) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    self.buffer = Vec::new();
+                } else {
+                    self.buffer = Vec::from(b);
+                }
+                Ok(&mut self.buffer)
+            },
+            Err(e) => {
+                Err(e)
+            },
+        }
+    }
+
+    fn consume(&mut self, amt: usize) {
+        if amt > 0 && amt <= self.buffer.len() {
+            self.buffer = self.buffer.split_off(amt);
+        }
+    }
+}
+
+fn parse_file_with_string(rio_ring: &rio::Rio, fd: File, path: &str, substr: &str) -> Vec<String> {
+    println!("Parse {:?}", path);
     std::iter::once(path.to_string()).chain(
-        BufReader::new(fd).lines()
+        BufReader::new(RioFile::from(fd, rio_ring)).lines()
             .take_while(|line| line.is_ok())
             .filter_map(|line| {
                 let txt = line.unwrap();
@@ -75,7 +131,7 @@ fn parse_file_with_string(fd: File, path: &str, substr: &str) -> Vec<String> {
         .collect::<Vec<String>>()
 }
 
-fn spawn_parser_thread(rx_parse: Receiver<jwalk::DirEntry<((), ())>>, substr: String, tx_output: Sender<Vec<String>>, matching_files_only: bool) -> JoinHandle<()> {
+fn spawn_parser_thread(rio_ring: rio::Rio, rx_parse: Receiver<jwalk::DirEntry<((), ())>>, substr: String, tx_output: Sender<Vec<String>>, matching_files_only: bool) -> JoinHandle<()> {
     thread::spawn(move || {
         while let Ok(entry) = rx_parse.recv() {
             let path = entry.path();
@@ -88,7 +144,7 @@ fn spawn_parser_thread(rx_parse: Receiver<jwalk::DirEntry<((), ())>>, substr: St
                         }
                     }
                 } else {
-                    let out = parse_file_with_string(file, path.to_str().unwrap_or(""), &substr);
+                    let out = parse_file_with_string(&rio_ring, file, path.to_str().unwrap_or(""), &substr);
                     if out.len() > 1 {
                         match tx_output.send(out) {
                             Err(e) => println!("Error to send output to displayer: {:?}", e),
@@ -104,6 +160,8 @@ fn spawn_parser_thread(rx_parse: Receiver<jwalk::DirEntry<((), ())>>, substr: St
 }
 
 fn main() {
+    let rio_ring = rio::new().expect("Cannot create uring - is it supported?");
+
     let args = Args::parse();
     let concurrency_multiplier = args.concurrency_multiplier.unwrap_or(2);
     let num_parsers = num_cpus::get() * concurrency_multiplier;
@@ -128,7 +186,7 @@ fn main() {
     let parse_threads = {
         let mut t = Vec::new();
         for _ in 0..num_parsers {
-            t.push(spawn_parser_thread(rx_files.clone(), substr.to_string(), tx_output.clone(), matching_files_only));
+            t.push(spawn_parser_thread(rio_ring.clone(), rx_files.clone(), substr.to_string(), tx_output.clone(), matching_files_only));
         }
 
         t
